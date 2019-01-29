3917b76ad7820c55fe0c36332358ffed199963c5
unknown <you@example.com>
2019-01-29 16:13:44 +0800

    init foods

--- src/pages/Login/Login.vue
+++ src/pages/Login/Login.vue
@@ -4,16 +4,18 @@
         <div class="login_header">
           <h2 class="login_logo">硅谷外卖</h2>
           <div class="login_header_title">
-            <a href="javascript:;" class="on">短信登录</a>
-            <a href="javascript:;">密码登录</a>
+            <a href="javascript:;" :class="{on:loginWay}" @click="loginWay=true">短信登录</a>
+            <a href="javascript:;" :class="{on:!loginWay}" @click="loginWay=false">密码登录</a>
           </div>
         </div>
         <div class="login_content">
           <form>
-            <div class="on">
+            <div :class="{on:loginWay}">
               <section class="login_message">
-                <input type="tel" maxlength="11" placeholder="手机号">
-                <button disabled="disabled" class="get_verification">获取验证码</button>
+                <input type="tel" maxlength="11" placeholder="手机号" v-model="phone">
+                <button :disabled="!rightPhone" class="get_verification" :class="{right_phone:rightPhone}" @click.prevent="getCode">
+					{{computeTime>0 ? `已发送(${computeTime}秒)` : '获取验证码'}}
+				</button>
               </section>
               <section class="login_verification">
                 <input type="tel" maxlength="8" placeholder="验证码">
@@ -23,7 +25,7 @@
                 <a href="javascript:;">《用户服务协议》</a>
               </section>
             </div>
-            <div>
+            <div :class="{on:!loginWay}">
               <section>
                 <section class="login_message">
                   <input type="tel" maxlength="11" placeholder="手机/邮箱/用户名">
@@ -56,7 +58,34 @@
 
 
 export default {
-	
+	data () {
+		return {
+			loginWay:true,
+			computeTime:0,
+			phone:'',
+		}
+	},
+	computed:{
+		rightPhone(){
+			return /^1\d{10}$/.test(this.phone)
+		}
+	},
+	methods:{
+		getCode(){
+			if(!this.computeTime){
+				this.computeTime = 30
+				const intervalId = setInterval(() => {
+					this.computeTime--
+					if(this.computeTime <= 0){
+						clearInterval(intervalId)
+					}
+				},1000)
+			}
+			
+			
+			
+		}
+	}
  
 }
 </script>
@@ -114,6 +143,7 @@ export default {
                 font-size 14px
                 background #fff
                 .get_verification
+                  outline 0
                   position absolute
                   top 50%
                   right 10px
@@ -122,6 +152,8 @@ export default {
                   color #ccc
                   font-size 14px
                   background transparent
+                  &.right_phone
+                    color black
               .login_verification
                 position relative
                 margin-top 16px
